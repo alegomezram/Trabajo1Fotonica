ddbfea23608512267d150d47ce5c0977446a0c5e
alegomezram <alegomezram@unal.edu.co>
2022-04-16 15:08:00 -0500

    Nuevo modo

--- MetodoRayosTM.m
+++ MetodoRayosTM.m
@@ -17,12 +17,13 @@ S0=@(theta) 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n
 S1=@(theta) pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
 S2=@(theta) 2*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
 S3=@(theta) 3*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
+S4=@(theta) 4*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
 
 %Plot
 theta=linspace(0,pi/2,1000);
-figure('Name','Modos TE'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
-    theta,S2(theta),theta,S3(theta),'LineWidth',1.5), grid on
-legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','Location','best')
-title('Modos TE')
+figure('Name','Modos TM'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
+    theta,S2(theta),theta,S3(theta),theta,S4(theta),'LineWidth',1.5), grid on
+legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','S(m=4)','Location','best')
+title('Modos TM')
 xlabel('theta [rad]') 
 ylabel('R(θ),S(θ)')
