850f35206dbc58836d2420a84ba8918864b423b7
alegomezram <alegomezram@unal.edu.co>
2022-04-16 14:59:22 -0500

    Mismo código para TM

---
+++ MetodoRayosTM.m
@@ -0,0 +1,28 @@
+clc, close all, clear all
+
+% Método de rayos
+% modos TM
+
+%Parámetros
+%Todas las unidades en um
+n1=1.5;  %core
+n2=1;    %cladding
+h=1;     %grosor del core
+lambda=1; %longitud de onda
+k0=2*pi/lambda;  %numero de onda
+
+%Ecuación de dispersión
+R=@(theta) h*k0*n1.*cos(theta);
+S0=@(theta) 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
+S1=@(theta) pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
+S2=@(theta) 2*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
+S3=@(theta) 3*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
+
+%Plot
+theta=linspace(0,pi/2,1000);
+figure('Name','Modos TE'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
+    theta,S2(theta),theta,S3(theta),'LineWidth',1.5), grid on
+legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','Location','best')
+title('Modos TE')
+xlabel('theta [rad]') 
+ylabel('R(θ),S(θ)')
