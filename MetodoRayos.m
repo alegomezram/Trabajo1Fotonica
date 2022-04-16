fd4233b42094467c81411b9f86909d7f4c147028
alegomezram <alegomezram@unal.edu.co>
2022-04-16 14:25:06 -0500

    Definición de funciones

--- MetodoRayos.m
+++ MetodoRayos.m
@@ -12,5 +12,10 @@ lambda=1; %longitud de onda
 k0=2*pi/lambda;  %numero de onda
 
 %Ecuación de dispersión
-R=h*k0*n1*cos(theta);
-S=pi+2*atan((sqrt((n1^2)*((sin(theta))^2)-(n2^2)))/(n1cos(theta)));
+R=@(theta) h*k0*n1*cos(theta);
+S=@(theta) pi+2*atan((sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))/(n1*cos(theta)));
+
+%Plot
+theta=linspace(0,pi/2,10);
+plot(theta,R(theta),theta,S(theta)), grid on
+legend('R','S','Location','best')
