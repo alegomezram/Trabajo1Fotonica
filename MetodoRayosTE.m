10f7fecce3bce92cdef8f9a302810253274bf318
alegomezram <alegomezram@unal.edu.co>
2022-04-16 19:59:31 -0500

    Graficar

--- MetodoRayosTE.m
+++ MetodoRayosTE.m
@@ -18,14 +18,14 @@ S1=@(theta) pi + 2.*atan((sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta
 S2=@(theta) 2*pi + 2.*atan((sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
 S3=@(theta) 3*pi + 2.*atan((sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
 
-% %Plot
-% theta=linspace(0,pi/2,1000);
-% figure('Name','Modos TE'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
-%     theta,S2(theta),theta,S3(theta),'LineWidth',1.5), grid on
-% legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','Location','best')
-% title('Modos TE')
-% xlabel('theta [rad]') 
-% ylabel('R(θ),S(θ)')
+%Plot
+theta=linspace(0,pi/2,1000);
+figure('Name','Modos TE'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
+    theta,S2(theta),theta,S3(theta),'LineWidth',1.5), grid on
+legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','Location','best')
+title('Modos TE')
+xlabel('theta [rad]') 
+ylabel('R(θ),S(θ)')
 % 
 % %Ecuación de dispersión
 % syms theta
@@ -38,11 +38,11 @@ S3=@(theta) 3*pi + 2.*atan((sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(the
 % %Método de Newton para hallar las soluciones (intersecciones de las gráficas)
 % f = r-s1;
 % df = diff(f)
-f=@(theta) h*k0*n1.*cos(theta) - 3*pi -2.*atan((sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
-df=@(theta) - 3*pi*sin(theta) - (2*((3*sin(theta))/(2*((9*sin(theta)^2)/4 - 1)^(1/2)) ...
-    + (2*sin(theta)*((9*sin(theta)^2)/4 - 1)^(1/2))/(3*cos(theta)^2)))/((4*((9*sin(theta)^2)/4 - 1))/(9*cos(theta)^2) + 1);
-p0= 1;
-delta=1e-6;
-epsilon=1e-6;
-max=20;
-[p0, err, k, y] = newton(f, df, p0, delta, epsilon, max) 
+% f=@(theta) h*k0*n1.*cos(theta) - 3*pi -2.*atan((sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
+% df=@(theta) - 3*pi*sin(theta) - (2*((3*sin(theta))/(2*((9*sin(theta)^2)/4 - 1)^(1/2)) ...
+%     + (2*sin(theta)*((9*sin(theta)^2)/4 - 1)^(1/2))/(3*cos(theta)^2)))/((4*((9*sin(theta)^2)/4 - 1))/(9*cos(theta)^2) + 1);
+% p0= 1;
+% delta=1e-6;
+% epsilon=1e-6;
+% max=20;
+% [p0, err, k, y] = newton(f, df, p0, delta, epsilon, max) 
--- MetodoRayosTM.m
+++ MetodoRayosTM.m
@@ -19,14 +19,14 @@ S2=@(theta) 2*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2
 S3=@(theta) 3*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
 S4=@(theta) 4*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
 
-% %Plot
-% theta=linspace(0,pi/2,1000);
-% figure('Name','Modos TM'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
-%     theta,S2(theta),theta,S3(theta),theta,S4(theta),'LineWidth',1.5), grid on
-% legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','S(m=4)','Location','best')
-% title('Modos TM')
-% xlabel('theta [rad]') 
-% ylabel('R(θ),S(θ)')
+%Plot
+theta=linspace(0,pi/2,1000);
+figure('Name','Modos TM'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
+    theta,S2(theta),theta,S3(theta),'LineWidth',1.5), grid on
+legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','S(m=4)','Location','best')
+title('Modos TM')
+xlabel('theta [rad]') 
+ylabel('R(θ),S(θ)')
 
 %Ecuación de dispersión
 % syms theta
@@ -39,11 +39,11 @@ S4=@(theta) 4*pi + 2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2
 % % %Método de Newton para hallar las soluciones (intersecciones de las gráficas)
 % f = r-s0;
 % df = diff(f)
-f=@(theta) h*k0*n1.*cos(theta)-3*pi -2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
-df=@(theta) - 3*pi*sin(theta) - (2*((27*sin(theta))/(8*((9*sin(theta)^2)/4 - 1)^(1/2)) + ...
-    (3*sin(theta)*((9*sin(theta)^2)/4 - 1)^(1/2))/(2*cos(theta)^2)))/((9*((9*sin(theta)^2)/4 - 1))/(4*cos(theta)^2) + 1);
-p0= 0.7;
-delta=1e-6;
-epsilon=1e-6;
-max=20;
-[p0, err, k, y] = newton(f, df, p0, delta, epsilon, max) 
+% f=@(theta) h*k0*n1.*cos(theta)-3*pi -2.*atan(((n1^2)/(n2^2)).*(sqrt((n1^2)*((sin(theta)).^2)-(n2^2)))./(n1.*cos(theta)));
+% df=@(theta) - 3*pi*sin(theta) - (2*((27*sin(theta))/(8*((9*sin(theta)^2)/4 - 1)^(1/2)) + ...
+%     (3*sin(theta)*((9*sin(theta)^2)/4 - 1)^(1/2))/(2*cos(theta)^2)))/((9*((9*sin(theta)^2)/4 - 1))/(4*cos(theta)^2) + 1);
+% p0= 0.7;
+% delta=1e-6;
+% epsilon=1e-6;
+% max=20;
+% [p0, err, k, y] = newton(f, df, p0, delta, epsilon, max) 
