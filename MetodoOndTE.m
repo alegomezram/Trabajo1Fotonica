9441c394f16008bcb33956b291390f31844ca995
alegomezram <alegomezram@unal.edu.co>
2022-04-16 20:00:19 -0500

    Método ondulatorio

---
+++ MetodoOndTE.m
@@ -0,0 +1,12 @@
+clc, close all, clear all
+
+% Método de rayos
+% modos TE
+
+%Parámetros
+%Todas las unidades en um
+n1=1.5;  %core
+n2=1;    %cladding
+h=1;     %grosor del core
+lambda=1; %longitud de onda
+k0=2*pi/lambda;  %numero de onda
