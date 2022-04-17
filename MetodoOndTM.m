852ef621cce5226a6c371c705895094f33af06a0
alegomezram <alegomezram@unal.edu.co>
2022-04-17 12:36:31 -0500

    Encontrar ángulos

---
+++ MetodoOndTM.m
@@ -0,0 +1,32 @@
+clc, close all, clear all
+
+% Método de rayos
+% modos TE
+
+%Parámetros
+%Todas las unidades en um
+n1=1.5;  %core
+n2=1;    %cladding
+h=1;     %grosor del core
+lambda=1; %longitud de onda
+k0=2*pi/lambda;  %numero de onda
+
+%Ecuación de dispersión
+% beta=@(theta) k0*n1*sin(theta);
+% gammax=sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2));
+% kx=sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2);
+R=@(theta) h*k0*n1.*cos(theta);
+S0=@(theta) -2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+S1=@(theta) pi - 2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+S2=@(theta) 2*pi - 2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+S3=@(theta) 3*pi - 2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+
+%Plot
+theta=linspace(0,pi/2,1000);
+theta2=linspace(0.7294,pi/2,1000);
+figure('Name','Modos TE'), plot(theta,R(theta),theta2,S0(theta2),theta2,S1(theta2), ...
+    theta2,S2(theta2),theta2,S3(theta2),'LineWidth',1.5), grid on
+legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','Location','best')
+title('Modos TE')
+xlabel('theta [rad]') 
+ylabel('R(θ),S(θ)')
