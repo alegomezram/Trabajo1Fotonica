da88ce99e490c004b0a34f25cbdac941810131e3
alegomezram <alegomezram@unal.edu.co>
2022-04-17 13:47:54 -0500

    Cálculo de ángulos para modos pares

--- MetodoOndTE.m
+++ MetodoOndTE.m
@@ -22,33 +22,32 @@ S0=@(theta) atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^
 S1=@(theta) pi + atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
 S2=@(theta) 2*pi + atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
 
-%Plot
-theta=linspace(0,pi/2,1000);
-figure('Name','Modos TE'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
-    theta,S2(theta),'LineWidth',1.5), grid on
-legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','Location','best')
-title('Modos TE')
-xlabel('theta [rad]') 
-ylabel('R(θ),S(θ)')
+% %Plot
+% theta=linspace(0,pi/2,1000);
+% figure('Name','Modos TE'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
+%     theta,S2(theta),'LineWidth',1.5), grid on
+% legend('R','S(m=0)','S(m=1)','S(m=2)','S(m=3)','Location','best')
+% title('Modos TE')
+% xlabel('theta [rad]') 
+% ylabel('R(θ),S(θ)')
 
 % Ecuación de dispersión
 % syms theta
-% r=h*k0*n1.*cos(theta);
-% s0=-2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
-% s1=pi - 2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
-% s2=2*pi - 2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
-% s3=3*pi - 2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+% r=(h/2)*k0*n1.*cos(theta);
+% s0=atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+% s1=pi + atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+% s2=2*pi + atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
 % 
-%Método de Newton para hallar las soluciones (intersecciones de las gráficas)
+% % Método de Newton para hallar las soluciones (intersecciones de las gráficas)
 % f = r-s1;
 % df = diff(f)
-% f=@(theta) h*k0*n1.*cos(theta) -3*pi +2.*atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
-% df=@(theta) - (2*((9*pi^2*cos(theta)*sin(theta))/((9*pi^2*sin(theta)^2 - 2778046668940015/70368744177664)^(1/2)*(3125302502557517/35184372088832 - 9*pi^2*sin(theta)^2)^(1/2)) + (9*pi^2*cos(theta)*sin(theta)*(9*pi^2*sin(theta)^2 - 2778046668940015/70368744177664)^(1/2))/(3125302502557517/35184372088832 - 9*pi^2*sin(theta)^2)^(3/2)))/((9*pi^2*sin(theta)^2 - 2778046668940015/70368744177664)/(9*pi^2*sin(theta)^2 - 3125302502557517/35184372088832) - 1) - 3*pi*sin(theta);
-% p0= 1;
-% delta=1e-6;
-% epsilon=1e-6;
-% max=20;
-% [p0, err, k, y] = newton(f, df, p0, delta, epsilon, max) 
+f=@(theta) (h/2)*k0*n1.*cos(theta) -2*pi - atan((sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2)))./(sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2)));
+df=@(theta)((9*pi^2*cos(theta)*sin(theta))/((9*pi^2*sin(theta)^2 - 2778046668940015/70368744177664)^(1/2)*(3125302502557517/35184372088832 - 9*pi^2*sin(theta)^2)^(1/2)) + (9*pi^2*cos(theta)*sin(theta)*(9*pi^2*sin(theta)^2 - 2778046668940015/70368744177664)^(1/2))/(3125302502557517/35184372088832 - 9*pi^2*sin(theta)^2)^(3/2))/((9*pi^2*sin(theta)^2 - 2778046668940015/70368744177664)/(9*pi^2*sin(theta)^2 - 3125302502557517/35184372088832) - 1) - (3*pi*sin(theta))/2;
+p0= 1;
+delta=1e-6;
+epsilon=1e-6;
+max=20;
+[p0, err, k, y] = newton(f, df, p0, delta, epsilon, max) 
 
 
 %IMPARES
@@ -57,12 +56,12 @@ ylabel('R(θ),S(θ)')
 % beta=@(theta) k0*n1*sin(theta);
 % gammax=sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2));
 % kx=sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2);
-R=@(theta) h*k0*n1.*cos(theta);
-S0=@(theta) -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
-S1=@(theta) pi -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
-S2=@(theta) 2*pi -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
-S3=@(theta) 3*pi -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
-% 
+% R=@(theta) h*k0*n1.*cos(theta);
+% S0=@(theta) -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
+% S1=@(theta) pi -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
+% S2=@(theta) 2*pi -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
+% S3=@(theta) 3*pi -2.*atan((sqrt((n1^2)*(k0^2) -(k0*n1*sin(theta)).^2))./(sqrt((k0*n1*sin(theta)).^2 -(n2^2)*(k0^2))));
+% % 
 % %Plot
 % theta=linspace(0,pi/2,1000);
 % figure('Name','Modos TE'), plot(theta,R(theta),theta,S0(theta),theta,S1(theta), ...
